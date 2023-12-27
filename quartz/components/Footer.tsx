898d3f2ca33e0a39dbf00fb41e99d4612350993f
winnermale <efecanozdemir@gmail.com>
2023-12-28 02:26:08 +0300

    Quartz sync: Dec 28, 2023, 2:26 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -11,20 +11,6 @@ export default ((opts?: Options) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
-      <footer class={`${displayClass ?? ""}`}>
-        <hr />
-        <p>
-          Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>, © {year}
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
-      </footer>
-    )
   }
 
   Footer.css = style
