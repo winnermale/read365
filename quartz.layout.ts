93e14a43c4b8584a05a7389ed613f6e944347e02
winnermale <efecanozdemir@gmail.com>
2023-12-29 17:07:09 +0300

    Quartz sync: Dec 29, 2023, 5:07 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -36,7 +36,36 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
     // Component.Explorer(),
     // Component.Backlinks(),
-    (Component.Graph()),
+    (Component.Graph(
+      {
+        localGraph: {
+          drag: true, // whether to allow panning the view around
+          zoom: true, // whether to allow zooming in and out
+          depth: 1, // how many hops of notes to display
+          scale: 5, // default view scale
+          repelForce: 0.5, // how much nodes should repel each other
+          centerForce: 1, // how much force to use when trying to center the nodes
+          linkDistance: 10, // how long should the links be by default?
+          fontSize: 0.2, // what size should the node labels be?
+          opacityScale: 5, // how quickly do we fade out the labels when zooming out?
+          removeTags: [], // what tags to remove from the graph
+          showTags: true, // whether to show tags in the graph
+        },
+        globalGraph: {
+          drag: true,
+          zoom: true,
+          depth: -1,
+          scale: 3,
+          repelForce: 0.1,
+          centerForce: 1,
+          linkDistance: 10,
+          fontSize: 0.4,
+          opacityScale: 5,
+          removeTags: [], // what tags to remove from the graph
+          showTags: true, // whether to show tags in the graph
+        },
+      }
+    )),
   ],
 }
 
