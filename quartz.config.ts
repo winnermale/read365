f20d4847b719cff4a9cab4706d78a2194b6ccff9
winnermale <efecanozdemir@gmail.com>
2024-01-02 03:23:43 +0300

    Quartz sync: Jan 2, 2024, 3:23 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "read365.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
