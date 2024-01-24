81dbbfa273dd8c88dc7b1ad3c4c9c3d6a538a75e
winnermale <efecanozdemir@gmail.com>
2024-01-25 00:34:54 +0300

    Quartz sync: Jan 25, 2024, 12:34 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,7 +7,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: false,
     analytics: {
-      provider: "plausible",
+      provider: 'google', tagId: G-7GF3YCTR1R,
     },
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["Private", "Templates", "Utility", ".obsidian"],
